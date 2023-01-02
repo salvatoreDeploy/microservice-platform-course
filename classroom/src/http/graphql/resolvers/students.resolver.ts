9472ae57b676cd79fef49c3b33af8c5c41931dba
salvatoreDeploy <liderhenrique@gmail.com>
2023-01-01 23:31:44 -0300

    feat list info the user autheticated

--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -1,4 +1,7 @@
+import { UseGuards } from '@nestjs/common';
 import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/authorization/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/authorization/current-user';
 import { EnrrollmentService } from 'src/services/enrollments.service';
 import { StudentsService } from 'src/services/students.service';
 import { Enrollment } from '../models/enrollment';
@@ -18,5 +21,11 @@ export class StudentsResolver {
   @ResolveField()
   enrollments(@Parent() enrollment: Enrollment) {
     return this.enrollmentsService.listEnrollmnetByStudent(enrollment.id);
+  }
+
+  @Query(() => Student)
+  @UseGuards(AuthorizationGuard)
+  me(@CurrentUser() user: AuthUser) {
+    return this.studensService.getStudentByAuthUserId(user.sub);
   }
 }

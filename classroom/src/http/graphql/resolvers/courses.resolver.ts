6c2befbdc66fdb01f3dcd38ba555e40740250fde
salvatoreDeploy <liderhenrique@gmail.com>
2023-01-01 22:11:35 -0300

    feat: Create new course

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,5 +1,6 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CoursesService } from 'src/services/courses.service';
+import { CreateCourseInput } from '../inputs/create-course-inputs';
 import { Course } from '../models/course';
 
 @Resolver(() => Course)
@@ -9,5 +10,10 @@ export class CoursesResolver {
   @Query(() => [Course])
   courses() {
     return this.courseService.listAllCourses();
+  }
+
+  @Mutation(() => Course)
+  createCourse(@Args('data') data: CreateCourseInput) {
+    return this.courseService.createCourse(data);
   }
 }
